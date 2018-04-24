35c100e49c3d093bcbc51cb94b7c7ea75fd5256a
Th3Chos3nOn3 <37116470+Th3Chos3nOn3@users.noreply.github.com>
2018-04-24 23:01:47 +0200

    implemented resizing

---
+++ src/main/java/eu/unipv/projectk/FunctionManager.java
@@ -0,0 +1,15 @@
+package eu.unipv.projectk;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionController {
+    private List<Function<Double, Double>> functions;
+
+    public FunctionController() {
+        functions = new ArrayList<>();
+    }
+
+    public void add(Function<Double, Double> f)
+}
---
+++ src/main/java/eu/unipv/projectk/functions/FooMathFunction.java
@@ -0,0 +1,5 @@
+package eu.unipv.projectk;
+
+public interface FooMathFunction {
+    double apply(double x);
+}
