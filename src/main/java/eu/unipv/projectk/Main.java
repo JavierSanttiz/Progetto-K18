fe88e6a65249a4cd57fc36fea7e06ea6214394e7
Th3Chos3nOn3 <37116470+Th3Chos3nOn3@users.noreply.github.com>
2018-04-25 14:39:13 +0200

    prototyping input panel

--- src/main/java/eu/unipv/projectk/Main.java
+++ src/main/java/eu/unipv/projectk/Main.java
@@ -1,19 +1,27 @@
 package eu.unipv.projectk;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        CartesianPlane plane = new CartesianPlane(new FunctionManager(), -5, 5, -5, 5);
-        Scene scene = new Scene(plane, 600, 600);
+        StackPane root = new StackPane();
 
-        plane.addFunction(Math::sin);
+        CartesianPlane plane = new CartesianPlane(new FunctionManager(), -5, 5, -5, 5);
+        FunctionsInputPane functionsInputPane = new FunctionsInputPane();
+        StackPane.setAlignment(functionsInputPane, Pos.TOP_LEFT);
+
+        root.getChildren().addAll(plane, functionsInputPane);
+
+        plane.addFunction(Math::log);
         plane.plot();
 
+        Scene scene = new Scene(root, 600, 600);
         primaryStage.setTitle("Plotter");
         primaryStage.setScene(scene);
         primaryStage.show();
